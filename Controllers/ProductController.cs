23f4168e40bc6e3ee3ed307ad4817cabed5a3080
Numan <numan.mercan24@gmail.com>
2022-08-16 16:36:20 +0300

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekUygulama.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrnekUygulama.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        #region JSONResult
+        public JsonResult GetProducts()
+        {
+            JsonResult result = Json(new Product
+            {
+                id = 5,
+                ProductName = "Terlik",
+                Quantity = 15
+            });
+            return result;
+        }
+        #endregion
+
+        #region EmptyResult
+        public EmptyResult GetProduct()
+        {
+            return new EmptyResult();
+        }
+        #endregion
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrnekUygulama.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
