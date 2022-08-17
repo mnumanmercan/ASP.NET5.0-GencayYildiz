728bc7c4460366aec2f29235f9a375141ff4b79f
Numan <numan.mercan24@gmail.com>
2022-08-17 11:21:23 +0300

    Veri tasima kontrolleri eklendi

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using OrnekUygulama.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace OrnekUygulama.Controllers
@@ -26,21 +27,43 @@ namespace OrnekUygulama.Controllers
             #region Veri Taşıma Kontrolleri
 
             #region ViewBag
-
+            //View'e gonderilecek datayı dinamik tanımlı bir degiskenle taşınmasını saglayan kontrol
+            //ViewBag.products = products;
             #endregion
 
             #region ViewData
-
+            // ViewBag gibi actiondaki datayi view'e taşımayı saglar
+            // ViewBag'den farkı 
+            //ViewData["product"] = products;
             #endregion
+
 
             #region TempData
+            //Bu da viewData ya benzer
+            string data = JsonSerializer.Serialize(products); // bu sekilde veri json formata cevirilip kompleks olmaktan cıkar ve temp data ile yönlendirilen action da kullanılır 
+            TempData["products"] = data;
 
+            //TempData["x"] = 5;
+            //ViewBag.x = 5;
+            //ViewData["x"] = 5;
             #endregion
 
             #endregion
 
 
+            return RedirectToAction("Index2", "Product");
+        }
+
+        public IActionResult Index2()
+        {
+            // var v1 = ViewBag.x;
+            // var v2 = ViewData["x"];
+            // var v3 = TempData["x"];
+
+            var data = TempData["products"].ToString();
+            List<Product> products = JsonSerializer.Deserialize<List<Product>>(data);
             return View();
         }
+
     }
 }
