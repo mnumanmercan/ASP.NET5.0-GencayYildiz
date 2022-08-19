55fe8b7437a4e8cb8914a1d3e09995733056e864
Numan <numan.mercan24@gmail.com>
2022-08-19 11:33:35 +0300

    32 - ModelMetadataValidation for SOLID principles

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using OrnekUygulama.Models.ModelMetadataTypes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,13 +8,12 @@ using System.Threading.Tasks;
 
 namespace OrnekUygulama.Models
 {
+    [ModelMetadataType(typeof(ProductMetadata))]
     public class Product
     {
-        [Required(ErrorMessage = "Lutfen product name'i girin.")]
-        [StringLength(100,ErrorMessage ="En fazla 100 karakter giriniz")]
+       
         public string ProductName { get; set; }
         public int Quantity { get; set; }
-        [EmailAddress(ErrorMessage ="Lutfen dogru bir email adresi giriniz.")]
         public string Email { get; set; }
     }
 }
